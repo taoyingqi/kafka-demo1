6bec36f0592aab431207d7e9c2776fe44d460879
taoyingqi <taoyingqi2013@sina.com>
2017-07-20 14:30:01 +0800

    New project

---
+++ src/main/java/youzidata-web/App.java
@@ -0,0 +1,13 @@
+package youzidata-web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
